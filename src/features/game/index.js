11bd9665687e113650170c40e2681c0ab024c0b5
omer.zahid@mnsu.edu <omer.zahid@mnsu.edu>
2025-10-08 21:51:08 -0700

    Removed redundent index.js becuase I useing Router and file path directly so no need of index.js pattren

--- src/features/game/index.js
+++
@@ -1,5 +0,0 @@
-import PlayGame from "./pages/PlayGame";
-
-export default function GameHome() {
-    return <PlayGame />;
-}
--- src/features/resume/index.js
+++
@@ -1,4 +0,0 @@
-import Home from "./pages/Home";
-export default function ResumeHome() {
-    return <Home />;
-}
